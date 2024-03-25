d2767608b13c9a33359ea71ac73f9f930ee7c0ea
kumar <apala3936@gmail.com>
2024-03-25 10:04:16 +0530

    adv java

---
+++ HelloWorld/src/main/java/HelloWorldApp/HelloWorldServlet.java
@@ -0,0 +1,23 @@
+package HelloWorldApp;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		PrintWriter writer = response.getWriter();
+		writer.println("The king of andhra kumar");
+		writer.println("Nithin naidu akula");
+		writer.println("ajay comedy star");
+	}
+
+}
